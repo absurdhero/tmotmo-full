f7694aaa900c016cde87367c08ffeda27ca4ec63
Lars Petrus <git@lar5.com>
2011-08-20 12:52:19 -0700

    more files

---
+++ Tests/FooTest.cs
@@ -0,0 +1,13 @@
+using System;
+using NUnit.Framework;
+namespace Irrelevant
+{
+  [TestFixture()]
+  public class FooTest
+  {
+    [Test()]
+    public void TestCase() {
+    }
+  }
+}
+
---
+++ Tests/Test.cs
@@ -0,0 +1,31 @@
+using System;
+using NUnit.Framework;
+using NUnit.Framework.SyntaxHelpers;
+
+namespace Tests
+{
+  [TestFixture]
+  public class MusicPlayerTest
+  {
+    private Foo foo; 
+    [SetUp]
+    public void SFJKHSDFJKH()
+    {
+      foo = new Foo();
+    }
+    
+    [TearDown]
+    public void SDFJHDSF() 
+    {
+      fo3
+    }
+    
+    
+    [Test]
+    public void whenNoTracksPlaying() {
+      Assert.That(0, Is.Not.EqualTo(1));
+      Assert.That("bob", Text.Contains("ob"));
+    }
+  }
+}
+
