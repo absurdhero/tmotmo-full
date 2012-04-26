56c9cb63be95380f4e7970bd5f76be9e027c4d53
Brandon Bremen <bbremen@gmail.com>
2012-04-25 21:18:30 -0700

    Added mock Handheld and Mathf classes to MockUnity

---
+++ MockUnity/Handheld.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace UnityEngine
+{
+	public class Handheld
+	{
+		public static void Vibrate() {
+			throw new NotImplementedException();
+		}
+	}
+}
+
---
+++ MockUnity/Mathf.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace UnityEngine
+{
+	public static class Mathf
+	{
+		public static float Round (float f) {
+			throw new NotImplementedException();
+		}
+	}
+}
+
