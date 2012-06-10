4d190dfeb94bce3f4f1f56c42e71eb53a20d4d39
Brandon Bremen <bbremen@gmail.com>
2012-06-09 18:16:59 -0700

    In SceneNine, scroll the todo list up and split it into torn pieces.
    To be implemented:
    - Picking up the glitch pieces onto the list
    - Letting the user tear it apart

--- Assets/Scripts/Scenes/SceneNine.cs
+++ Assets/Scripts/Scenes/SceneNine.cs
@@ -5,11 +5,13 @@ public class SceneNine : Scene {
 	public Confetti confetti;
 
 	private UnityInput input;
+	GameObject background;
 	private TodoList todoList;
 
 	public SceneNine(SceneManager manager, Confetti confetti) : base(manager) {
 		this.confetti = confetti;
 		input = new UnityInput();
+		background = resourceFactory.Create("TodoList/GreenQuad");
 		todoList = new TodoList(resourceFactory);
 	}
 
@@ -24,26 +26,76 @@ public class SceneNine : Scene {
 
 	public override void Destroy () {
 		confetti.Destroy();
+		GameObject.Destroy(background);
 		todoList.Destroy();
 	}
 
 	public class TodoList {
-		GameObject background;
 		GameObjectFactory<string> resourceFactory;
+
+		const int VERTICAL_STOP_POSITION = 35;
+		const int SCROLL_SPEED = 20;
+
+		Metronome metronome;
+		GameObject intactTodoList;
+		Sprite intactTodoListSprite { get { return intactTodoList.GetComponent<Sprite>(); } }
+		GameObject tornListLeft, tornListRight, tornListBottom;
+		Sprite tornListLeftSprite { get { return tornListLeft.GetComponent<Sprite>(); } }
+		Sprite tornListRightSprite { get { return tornListRight.GetComponent<Sprite>(); } }
+		Sprite tornListBottomSprite { get { return tornListBottom.GetComponent<Sprite>(); } }
+		
+		bool tearable = false;
 		
 		public TodoList(GameObjectFactory<string> resourceFactory) {
 			this.resourceFactory = resourceFactory;
 		}
 		
 		public void Setup() {
-			background = resourceFactory.Create("TodoList/GreenQuad");
+			intactTodoList = resourceFactory.Create("TodoList/todo");
+			intactTodoListSprite.setScreenPosition(200, -intactTodoListSprite.height);
+			metronome = new Metronome(Time.time, 0.125f);
 		}
 
 		public void Update(float time) {
+			if (tearable) {
+				receiveTearingInput();
+				return;
+			}
+			if (intactTodoListSprite.getScreenPosition().y >= VERTICAL_STOP_POSITION) {
+				replaceListWithTornPieces();
+				return;
+			}
+			
+			if (metronome.isNextTick(time)) {
+				intactTodoListSprite.move(0, SCROLL_SPEED);
+			}
 		}
 		
 		public void Destroy() {
-			GameObject.Destroy(background);
+			GameObject.Destroy(intactTodoList);
 		}
+
+		public void replaceListWithTornPieces() {
+			tearable = true;
+			
+			tornListLeft = resourceFactory.Create("TodoList/tornListLeft");
+			tornListRight = resourceFactory.Create("TodoList/tornListRight");
+			tornListBottom = resourceFactory.Create("TodoList/tornListBottom");
+			
+			tornListLeft.transform.position = intactTodoList.transform.position;
+			tornListRight.transform.position = intactTodoList.transform.position;
+			tornListBottom.transform.position = intactTodoList.transform.position;
+
+			tornListLeftSprite.move(-10, 85);
+			tornListRightSprite.move(104, 110);
+			tornListBottomSprite.move(10, 0);
+			
+			intactTodoList.active = false;
+		}
+		
+		private void receiveTearingInput() {
+			
+		}
+
 	}
 }
