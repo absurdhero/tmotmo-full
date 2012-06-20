8f54aa32f1a7a02bd90596160543b8641e5114ae
Brandon Bremen <bbremen@gmail.com>
2012-06-19 19:56:44 -0700

    Refactored SceneTen to remove use of Repeater subclass for animation.
    Everything that was in the inner repeater class is now in SceneTen.
    The Metronome class is used to set the animation framerate.

--- Assets/Scripts/Scenes/SceneTen.cs
+++ Assets/Scripts/Scenes/SceneTen.cs
@@ -2,8 +2,25 @@ using UnityEngine;
 using System.Collections.Generic;
 
 public class SceneTen : Scene {
-	StompAnimator stompAnimator;
+	Metronome stompSpeed;
+	GameObject amp;
+	GameObject shoe;
+	GameObject wireShadow;
+	GameObject glassCracks;
+	GameObject wires;
+	GameObject background;
 	
+	Sprite ampSprite { get { return amp.GetComponent<Sprite>(); }}
+	Sprite shoeSprite { get { return shoe.GetComponent<Sprite>(); }}
+	Sprite glassSprite { get { return glassCracks.GetComponent<Sprite>(); }}
+	Sprite wiresSprite { get { return wires.GetComponent<Sprite>(); }}
+	Sprite wireShadowSprite { get { return wireShadow.GetComponent<Sprite>(); }}
+	
+	Animatable pedalStomped, pedalUnStomped, stompFoot, retractFoot;
+	Animatable[] animatables;
+	
+	const int firstCrackFrame = 3;
+
 	public SceneTen(SceneManager manager) : base(manager) {
 	}
 	
@@ -11,94 +28,64 @@ public class SceneTen : Scene {
 		timeLength = 2.0f;
 		endScene();
 		
-		stompAnimator = new StompAnimator(resourceFactory);
+		// double the scale on all of these because the art is half-size
+		background = resourceFactory.Create(this, "GreenBackground");
+		shoe = resourceFactory.Create("SceneTen/Shoe");
+		shoe.transform.localScale = Vector3.one * 2;
+		shoe.GetComponent<Sprite>().setScreenPosition(0, 164);
+		amp = resourceFactory.Create(this, "Amp");
+		amp.GetComponent<Sprite>().setScreenPosition(130, 50);
+		amp.transform.localScale = Vector3.one * 2;
+		wires = resourceFactory.Create(this, "Wires");
+		wires.GetComponent<Sprite>().setScreenPosition(30, 30);
+		wires.transform.localScale = Vector3.one * 2;			
+		wireShadow = resourceFactory.Create(this, "WireShadow");
+		wireShadow.GetComponent<Sprite>().setScreenPosition(30, 30);
+		wireShadow.transform.localScale = Vector3.one * 2;
+		
+		pedalStomped = new PedalStomped(new List<Sprite> {ampSprite, wiresSprite, wireShadowSprite});
+		pedalUnStomped = new PedalUnStomped(new List<Sprite> {ampSprite, wiresSprite, wireShadowSprite});
+		stompFoot = new StompFoot(new List<Sprite> {shoeSprite});
+		retractFoot = new RetractFoot(new List<Sprite> {shoeSprite});
+		animatables = new[] {pedalStomped, pedalUnStomped, stompFoot, retractFoot};
+		
+		stompSpeed = new Metronome(Time.time, 0.125f);
 	}
 
 	public override void Update () {
-		stompAnimator.Update(Time.time);
+		float time = Time.time;
+		if (stompSpeed.isNextTick(time)) {
+			OnTick(stompSpeed.currentTick(time));
+		}
 	}
 
 	public override void Destroy () {
-		stompAnimator.Destroy();
+		GameObject.Destroy(amp);
+		GameObject.Destroy(shoe);
+		GameObject.Destroy(wireShadow);
+		GameObject.Destroy(glassCracks);
+		GameObject.Destroy(wires);
+		GameObject.Destroy(background);
+	}
+			
+	public void beginCracks() {
+		glassCracks = resourceFactory.Create(this, "GlassCracks");
+		glassCracks.transform.localScale = Vector3.one * 2;
+		glassSprite.setScreenPosition(0, 0);
+		glassSprite.setDepth(0f);
 	}
 	
-	class StompAnimator : Repeater {
-		GameObjectFactory<string> resourceFactory;
-		
-		GameObject amp;
-		GameObject shoe;
-		GameObject wireShadow;
-		GameObject glassCracks;
-		GameObject wires;
-		GameObject background;
-		
-		Sprite ampSprite { get { return amp.GetComponent<Sprite>(); }}
-		Sprite shoeSprite { get { return shoe.GetComponent<Sprite>(); }}
-		Sprite glassSprite { get { return glassCracks.GetComponent<Sprite>(); }}
-		Sprite wiresSprite { get { return wires.GetComponent<Sprite>(); }}
-		Sprite wireShadowSprite { get { return wireShadow.GetComponent<Sprite>(); }}
-		
-		Animatable pedalStomped, pedalUnStomped, stompFoot, retractFoot;
-		Animatable[] animatables;
-		
-		const int firstCrackFrame = 3;
-
-		
-		public StompAnimator(GameObjectFactory<string> resourceFactory) : base(0.125f, 16) {
-			this.resourceFactory = resourceFactory;
-
-			// double the scale on all of these because the art is half-size
-			background = resourceFactory.Create("SceneTen/GreenBackground");
-			shoe = resourceFactory.Create("SceneTen/Shoe");
-			shoe.transform.localScale = Vector3.one * 2;
-			shoe.GetComponent<Sprite>().setScreenPosition(0, 164);
-			amp = resourceFactory.Create("SceneTen/Amp");
-			amp.GetComponent<Sprite>().setScreenPosition(130, 50);
-			amp.transform.localScale = Vector3.one * 2;
-			wires = resourceFactory.Create("SceneTen/Wires");
-			wires.GetComponent<Sprite>().setScreenPosition(30, 30);
-			wires.transform.localScale = Vector3.one * 2;			
-			wireShadow = resourceFactory.Create("SceneTen/WireShadow");
-			wireShadow.GetComponent<Sprite>().setScreenPosition(30, 30);
-			wireShadow.transform.localScale = Vector3.one * 2;
-			
-			pedalStomped = new PedalStomped(new List<Sprite> {ampSprite, wiresSprite, wireShadowSprite});
-			pedalUnStomped = new PedalUnStomped(new List<Sprite> {ampSprite, wiresSprite, wireShadowSprite});
-			stompFoot = new StompFoot(new List<Sprite> {shoeSprite});
-			retractFoot = new RetractFoot(new List<Sprite> {shoeSprite});
-			animatables = new[] {pedalStomped, pedalUnStomped, stompFoot, retractFoot};
-				
-		}
-		
-		public void beginCracks() {
-			glassCracks = resourceFactory.Create("SceneTen/GlassCracks");
-			glassCracks.transform.localScale = Vector3.one * 2;
-			glassSprite.setScreenPosition(0, 0);
-			glassSprite.setDepth(0f);
-		}
-		
-		public override void OnTick() {
-			foreach(var animatable in animatables) {
-				animatable.animate(currentTick);
-			}
-
-			if (currentTick == firstCrackFrame) {
-				beginCracks();
-			}
-			
-			if (currentTick > firstCrackFrame) {
-				glassSprite.DrawNextFrame();
-			}
+	private void OnTick(int currentTick) {
+		foreach(var animatable in animatables) {
+			animatable.animate(currentTick);
 		}
 
-
-		public void Destroy() {
-			GameObject.Destroy(amp);
-			GameObject.Destroy(shoe);
-			GameObject.Destroy(wireShadow);
-			GameObject.Destroy(glassCracks);
-			GameObject.Destroy(wires);
-			GameObject.Destroy(background);
+		if (currentTick == firstCrackFrame) {
+			beginCracks();
+		}
+		
+		if (currentTick > firstCrackFrame) {
+			glassSprite.DrawNextFrame();
 		}
 	}
 
@@ -125,5 +112,4 @@ public class SceneTen : Scene {
 
 		public RetractFoot(ICollection<Sprite> sprites) : base(sprites, ascent, 4, 8) {}
 	}
-
 }
