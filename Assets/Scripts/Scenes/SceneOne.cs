bdfd7e22b12393056beab99646914047ba7a4b9a
Brandon Bremen <bbremen@gmail.com>
2012-10-18 10:08:39 -0700

    Use Sprite instead of GameObject in SceneOne

--- Assets/Scripts/Scenes/SceneOne.cs
+++ Assets/Scripts/Scenes/SceneOne.cs
@@ -3,10 +3,10 @@ using System;
 
 class SceneOne : Scene {
 	public GameObject background;
-	public GameObject same;
-	public GameObject notSame;
-	public GameObject circle;
-	public GameObject triangle;
+	public Sprite same;
+	public Sprite notSame;
+	public Sprite circle;
+	public Sprite triangle;
 	
 	Wiggle wiggle;
 
@@ -28,34 +28,33 @@ class SceneOne : Scene {
 
 	public override void LoadAssets() {
 		background = resourceFactory.Create("TitleScene/BackgroundQuad");
-		same = resourceFactory.Create(this, "Same");
-		notSame = resourceFactory.Create(this, "NotSame");
-		circle = resourceFactory.Create(this, "Circle");
-		triangle = resourceFactory.Create(this, "Triangle");
-
 		background.active = false;
-		same.active = false;
-		notSame.active = false;
-		circle.active = false;
-		triangle.active = false;
+
+		same = resourceFactory.Create(this, "Same").GetComponent<Sprite>();
+		notSame = resourceFactory.Create(this, "NotSame").GetComponent<Sprite>();
+		circle = resourceFactory.Create(this, "Circle").GetComponent<Sprite>();
+		triangle = resourceFactory.Create(this, "Triangle").GetComponent<Sprite>();
+
+		same.visible(false);
+		notSame.visible(false);
+		circle.visible(false);
+		triangle.visible(false);
 	}
 
 	public override void Setup(float startTime) {
 		background.active = true;
-		same.active = true;
-		notSame.active = true;
-		circle.active = true;
-		triangle.active = true;
+		same.visible(true);
+		notSame.visible(true);
+		circle.visible(true);
+		triangle.visible(true);
 
-		same.GetComponent<Sprite>().setCenterToViewportCoord(0.35f, 0.66f);
-		notSame.GetComponent<Sprite>().setCenterToViewportCoord(0.7f, 0.66f);
-		var circleSprite = circle.GetComponent<Sprite>();
-		var triangleSprite = triangle.GetComponent<Sprite>();
-		circleSprite.setCenterToViewportCoord(0.3f, 0.33f);
-		triangleSprite.setCenterToViewportCoord(0.7f, 0.33f);
+		same.setCenterToViewportCoord(0.35f, 0.66f);
+		notSame.setCenterToViewportCoord(0.7f, 0.66f);
+		circle.setCenterToViewportCoord(0.3f, 0.33f);
+		triangle.setCenterToViewportCoord(0.7f, 0.33f);
 		
 		// hide the triangle to start
-		triangle.active = false;
+		triangle.visible(false);
 		
 		circleCycler = new Cycler(shapeSpeed, 0, startTime);
 		circleCycler.AddSprite(circle);
@@ -63,14 +62,14 @@ class SceneOne : Scene {
 		notSameCycler = new DelayedCycler(0.2f, 4, 1.2f, startTime);
 		notSameCycler.AddSprite(notSame);
 
-		wiggle = new Wiggle(startTime, timeLength, new[] {circle.GetComponent<Sprite>(), triangle.GetComponent<Sprite>()});
+		wiggle = new Wiggle(startTime, timeLength, new[] {circle, triangle});
 	}
 
 	public override void Destroy() {
-		GameObject.Destroy(circle);
-		GameObject.Destroy(triangle);
-		GameObject.Destroy(same);
-		GameObject.Destroy(notSame);
+		Sprite.Destroy(circle);
+		Sprite.Destroy(triangle);
+		Sprite.Destroy(same);
+		Sprite.Destroy(notSame);
 		GameObject.Destroy(background);
 		wiggle.Destroy();
 	}
@@ -85,8 +84,8 @@ class SceneOne : Scene {
 		bool editorTouched = Application.isEditor && input.GetMouseButtonUp(0);
 		
 		if (editorTouched ||
-			(touch.insideSprite(Camera.main, circle.GetComponent<Sprite>()) &&
-			 touch.insideSprite(Camera.main, triangle.GetComponent<Sprite>()))) {
+			(touch.insideSprite(Camera.main, circle) &&
+			 touch.insideSprite(Camera.main, triangle))) {
 			Handheld.Vibrate();
 			wiggle.wiggleNow(now);
 			endScene();
@@ -99,7 +98,7 @@ class SceneOne : Scene {
 		circleCycler.Update(Time.time);
 		if (circleCycler.Count() == triangleWaitTime) {
 			triangleCycler = new DelayedCycler(shapeSpeed, 6, 1f);
-			triangle.active = true;
+			triangle.visible(true);
 			triangleCycler.AddSprite(triangle);
 		}
 		if (triangleCycler != null) {
